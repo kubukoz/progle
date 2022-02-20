85d0f26379060f75003d56ab0401c6c60a6fd549
Jakub Kozłowski <kubukoz@gmail.com>
2022-02-20 02:20:03 +0100

    fix shell on other systems

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
 let
+  args = if builtins.currentSystem == "aarch64-darwin" then { localSystem = "x86_64-darwin"; } else { };
   pkgs = import
     (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/c82b46413401efa740a0b994f52e9903a4f6dcd5.tar.gz";
     })
-    { localSystem = "x86_64-darwin"; };
+    args;
 in
 pkgs.mkShell {
   buildInputs = [ pkgs.scala-cli ];
