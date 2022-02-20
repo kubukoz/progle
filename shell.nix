3685df56de7f72fcea063ec6363dace82f75e73d
Jakub Kozłowski <kubukoz@gmail.com>
2022-02-20 02:20:38 +0100

    this should also work

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 let
-  args = if builtins.currentSystem == "aarch64-darwin" then { localSystem = "x86_64-darwin"; } else { };
+  localSystem = if builtins.currentSystem == "aarch64-darwin" then "x86_64-darwin" else null;
   pkgs = import
     (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/c82b46413401efa740a0b994f52e9903a4f6dcd5.tar.gz";
     })
-    args;
+    { inherit localSystem; };
 in
 pkgs.mkShell {
   buildInputs = [ pkgs.scala-cli ];
